86d216059bff95fef1aa1bf8d74a61ace4a6a592
flobo3 <floptopbot33@gmail.com>
2026-03-22 09:25:08 +0300

    fix(telegram): support chat_id:topic_id format

--- nanobot/channels/telegram.py
+++ nanobot/channels/telegram.py
@@ -362,6 +362,10 @@ class TelegramChannel(BaseChannel):
             parts = raw_chat_id.split("_")
             raw_chat_id = parts[0]
             message_thread_id = int(parts[1])
+        elif ":" in raw_chat_id:
+            parts = raw_chat_id.split(":")
+            raw_chat_id = parts[0]
+            message_thread_id = int(parts[1])
 
         try:
             chat_id = int(raw_chat_id)
